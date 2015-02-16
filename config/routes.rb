3808c7146ad0b15889c57f130e69f0e3c2ed3aa9
Chris Bouchard <chris@upliftinglemma.net>
2015-02-16 17:30:48 -0500

    Add main controller for the main site.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'main#index'
+
   post 'login' => 'sessions_controller#create'
   post 'logout' => 'sessions_controller#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
