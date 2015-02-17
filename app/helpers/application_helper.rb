e8c8996ebe08de111d5ef0fcb0565f56df60e799
Chris Bouchard <chris@upliftinglemma.net>
2015-02-16 21:48:47 -0500

    Get the basic layout up and running.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def content_or_default_for(name, default=nil)
+        if content_for?(name)
+            content_for(name)
+        else
+            default
+        end
+    end
 end
