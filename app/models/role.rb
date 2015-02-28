28b1e2a2cce25eb0aead723df9d9e2f1f74bf467
Chris Bouchard <chris@upliftinglemma.net>
2015-02-27 21:00:16 -0500

    Use the annotate gem to annotate models/fixtures/etc.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :integer          not null, primary key
+#  app_id     :integer          not null
+#  user_id    :integer          not null
+#  role       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Role < ActiveRecord::Base
     has_paper_trail
 
