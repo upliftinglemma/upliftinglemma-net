29d205319c3ee0443273312c814881a212549759
Chris Bouchard <chris@upliftinglemma.net>
2015-03-22 21:39:11 -0400

    Start using app decorators rather than helpers.

--- app/decorators/app_decorator.rb
+++ app/decorators/app_decorator.rb
@@ -1,10 +1,24 @@
 class AppDecorator < ApplicationDecorator
   include RendersMarkdown
 
+  decorates_association :owner
+
   delegate_all
+
+  def class_name
+    "app-#{slug}"
+  end
+
+  def class_for prop
+    %W(#{class_name} theme-#{prop})
+  end
 
   def rendered_description
     render_markdown "**#{name}** #{description}"
+  end
+
+  def stylesheet
+    "themes/#{slug}"
   end
 end
 
