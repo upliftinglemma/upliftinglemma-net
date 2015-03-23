b689a77d17e17f37a833b562b8a8d2c6a4810a78
Chris Bouchard <chris@upliftinglemma.net>
2015-03-22 23:54:03 -0400

    Correct formatting.

--- engines/blog/app/models/blog/article.rb
+++ engines/blog/app/models/blog/article.rb
@@ -1,49 +1,51 @@
 module Blog
-    class Article < ActiveRecord::Base
-        include ::Commentable
-        include ::FriendlyId
-        include ::Posted
+  class Article < ActiveRecord::Base
+    include ::Commentable
+    include ::FriendlyId
+    include ::Posted
 
-        belongs_to :blog, class_name: 'Blog::App'
+    belongs_to :blog, class_name: 'Blog::App'
 
-        friendly_id :slug_candidates, use: :scoped, scope: :blog
+    friendly_id :slug_candidates, use: :scoped, scope: :blog
 
-        validates_presence_of :title, :slug, :blog, :body
-        validate :blog_is_a_blog
-
-
-        def previous
-            self.class.first conditions: ['created_at < ?', created_at],
-                order: 'created_at desc'
-        end
-
-        def next
-            self.class.first conditions: ['created_at > ?', created_at],
-                order: 'created_at asc'
-        end
+    validates_presence_of :title, :slug, :blog, :body
+    validate :blog_is_a_blog
 
 
-        private
-
-        ##
-        # Validates that the app we're calling a blog is, in fact, a blog.
-
-        def blog_is_a_blog
-            blog.is_a? Blog::App
-        end
-
-
-        ##
-        # A list of possible attributes to incude in the slug. One of these
-        # should almost certainly be chosen, unless a user somehow posts two
-        # articles with the same title at the exact same time.
-
-        def slug_candidates
-            [
-                :title,
-                [:title, :created_at]
-            ]
-        end
+    def previous
+      self.class.where('created_at < ?', created_at)
+        .order(created_at: :desc)
+        .take
     end
+
+    def next
+      self.class.where('created_at > ?', created_at)
+        .order(created_at: :asc)
+        .take
+    end
+
+
+    private
+
+    ##
+    # Validates that the app we're calling a blog is, in fact, a blog.
+
+    def blog_is_a_blog
+      blog.is_a? Blog::App
+    end
+
+
+    ##
+    # A list of possible attributes to incude in the slug. One of these
+    # should almost certainly be chosen, unless a user somehow posts two
+    # articles with the same title at the exact same time.
+
+    def slug_candidates
+      [
+        :title,
+        [:title, :created_at]
+      ]
+    end
+  end
 end
 
